1b3a262ef48fc90e27802e24cb4ffe79b65787f4
ctorek <ctorek@protonmail.ch>
2021-12-11 23:10:07 -0500

    Finished chapter 4
    Completed the Scanner class with support for string literals, numeric
    literals, identifiers, and block comments.

--- jlox/src/main/java/jlox/Scanner.java
+++ jlox/src/main/java/jlox/Scanner.java
@@ -1,17 +1,46 @@
 package jlox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static jlox.Token.TokenType.*;
 
 public class Scanner {
+    // Map of keywords to tokens
+    private static final Map<String, Token.TokenType> keywords;
+
+    // Keywords are initialized only once for every instance of this class
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("fun", FUN);
+        keywords.put("for", FOR);
+        keywords.put("IF", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
+
+    // Source code being scanned
     private final String source;
+
+    // List of tokens resulting from scanned source code
     private final List<Token> tokens = new ArrayList<>();
 
-    private int start = 0;
-    private int current = 0;
-    private int line = 0;
+    private int start = 0; // Index of start of current token
+    private int current = 0; // Index of current character of current token
+    private int line = 0; // Current line number
 
     Scanner(String source) {
         this.source = source;
@@ -70,6 +99,8 @@ public class Scanner {
                 if(match('/')) {
                     // Continue until the end of the line if it is a comment
                     while (peek() != '\n' && !isAtEnd()) advance();
+                } else if (match('*')) {
+                    blockComment();
                 } else {
                     addToken(SLASH);
                 }
@@ -88,13 +119,141 @@ public class Scanner {
                 line++;
                 break;
 
-            /* Lox interpreter error if a token is not recognized */
+            /* String literals */
+            case '"': string(); break;
+
+            /* Default case handles number literals, identifiers, or unexpected chars */
             default:
-                Lox.error(line, "Unexpected character");
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    // Identifiers must not start with a number
+                    identifier();
+                } else {
+                    // Lox interpreter error if a token is not recognized
+                    Lox.error(line, "Unexpected character");
+                }
                 break;
         }
     }
 
+    /**
+     * Traverses an identifier containing alphanumeric characters. <br>
+     * Must start with an alphabetic character and not a number.
+     */
+    private void identifier() {
+        // Advance while the next character is another letter or number
+        while (isAlphanumeric(peek())) advance();
+
+        // Attempt to recognize as a keyword
+        String text = source.substring(start, current);
+        Token.TokenType type = keywords.get(text);
+
+        // Tokenized as an identifier if it is not a keyword
+        if (type == null) type = IDENTIFIER;
+
+        addToken(type);
+    }
+
+    /**
+     * Traverse a block comment without tokenizing any comment.
+     * Throws a Lox error if the file ends without closing the comment.
+     */
+    private void blockComment() {
+        // Before advancing, check that next chars are not end of comment
+        while (peek() != '*' && peekNext() != '/' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        // Throw error if end of file is found without closing comment
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated block comment");
+            return;
+        }
+
+        // Advance twice because there are two closing characters to block comments
+        advance(); advance();
+    }
+
+    /**
+     * Traverse a numeric literal and tokenizes content into a double. <br>
+     * Does not allow leading or trailing decimals.
+     */
+    private void number() {
+        // Advance while the next character is another digit of the number
+        while (isDigit(peek())) advance();
+
+        // Only consume a '.' if numbers come after it
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+            // Continue consuming numbers after '.' without allowing another '.'
+            while (isDigit(peek())) advance();
+        }
+
+        // Numbers are only represented as doubles in jlox and not integers
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    /**
+     * Traverse a string literal until the closing quote and tokenizes content. <br>
+     * Throws a Lox error if the string is not terminated by end of file. <br>
+     * Allows multiline strings.
+     */
+    private void string() {
+        // Before advancing, check that next char is not end of string
+        while (peek() != '"' && !isAtEnd()) {
+            // Advance line count if string contains newlines
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        // Throw error if end of file is found without closing quote
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string");
+            return;
+        }
+
+        // After the above loop and if statement, next char must be closing quote
+        advance();
+
+        // Get the value of the string without the quotes
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+    }
+
+    /**
+     * Check if a character is a number or other character
+     * @param c the character to check
+     * @return true if the character is a digit, false otherwise
+     */
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    /**
+     * Check if a character is a lowercase letter, uppercase letter, or underscore
+     * @param c the character to check
+     * @return true if the character is alphabetic, false otherwise
+     */
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                (c == '_');
+    }
+
+    /**
+     * Checks if a character is an alphabetic or numeric character. <br>
+     * Based on the {@link #isAlpha(char)} isAlpha} and {@link #isDigit(char) isDigit} methods.
+     */
+    private boolean isAlphanumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    /**
+     * Checks whether the current character being parsed is at the end of the file
+     * @return true if at end of file, false otherwise
+     */
     private boolean isAtEnd() {
         return current >= source.length();
     }
@@ -116,6 +275,20 @@ public class Scanner {
         return source.charAt(current);
     }
 
+    /**
+     * Returns the second next character to be parsed without moving onto the next
+     * @return char in source string at next index
+     */
+    private char peekNext() {
+        // Return null if peeking past the end of the source code
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    /**
+     * Adds a token to the list of tokens representing the scanned code
+     * @param type the type of token being added from {@link jlox.Token.TokenType}
+     */
     private void addToken(Token.TokenType type) {
         addToken(type, null);
     }
